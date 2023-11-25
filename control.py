ef1661a106fa91a85e8d24f01169c50bfe0a936e
Samarth Singhal <samarthssinghal@gmail.com>
2023-11-25 17:06:27 -0600

    added peer pressure(future me check the kaggle for what this is)

--- control.py
+++ control.py
@@ -10,19 +10,21 @@ def form():
 
 @app.route('/result', methods=['POST'])
 def result():
-    
+
     gender = request.form.get('gender')
     age = request.form.get('age')
     smoking = request.form.get('smoking')
     yf = request.form.get('yf')
     anxiety = request.form.get('anxiety')
+    peerpressure = request.form.get('peerpressure')
 
     return render_template('result.html',
         gender=gender, 
         age=age, 
         smoking=smoking,
         yf=yf,
-        anxiety=anxiety)
+        anxiety=anxiety,
+        peerpressure=peerpressure,)
 
 if __name__ == '__main__':
     app.run(debug=True)
