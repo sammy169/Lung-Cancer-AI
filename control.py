a8e2e33b7dd18a9afb7702c9960ee00daad00e4b
Samarth Singhal <samarthssinghal@gmail.com>
2023-11-25 17:02:44 -0600

    added too much of anxiety, not good for you

--- control.py
+++ control.py
@@ -10,15 +10,19 @@ def form():
 
 @app.route('/result', methods=['POST'])
 def result():
+    
     gender = request.form.get('gender')
     age = request.form.get('age')
     smoking = request.form.get('smoking')
     yf = request.form.get('yf')
+    anxiety = request.form.get('anxiety')
+
     return render_template('result.html',
         gender=gender, 
         age=age, 
         smoking=smoking,
-        yf=yf)
+        yf=yf,
+        anxiety=anxiety)
 
 if __name__ == '__main__':
     app.run(debug=True)
